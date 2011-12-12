5582d8ac07128b604c4084774a491258a262c22f
Niklas Hofer <niklas+dev@lanpartei.de>
2011-12-12 12:34:52 +0100

    provide a layout with hooks for action views

---
+++ app/helpers/jquery_mobile/tag_helper.rb
@@ -0,0 +1,43 @@
+module JqueryMobile::TagHelper
+
+
+  # put this into your layout. It will yield your action-renderings for you
+  def mobile_page(options = {})
+    @page_options = options
+    render 'mobile_page'
+  end
+
+  # adds the block contents to the JqueryMobile header
+  def for_header(options={}, &block)
+    @header_options = options
+    content_for :header, &block
+  end
+
+  # adds the block contents to the JqueryMobile footer
+  def for_footer(options={}, &block)
+    @footer_options = options
+    content_for :footer, &block
+  end
+
+  attr_reader :page_options
+  attr_reader :header_options
+  attr_reader :content_options
+  attr_reader :footer_options
+
+  # Renders a JqueryMobile specific tag
+  # addition options
+  #   :role   - JQM role (page, header, footer, etc)
+  #   :theme  - single letter color swatches (a,b or even d),
+  #             see http://jquerymobile.com/demos/1.0/docs/api/themes.html
+  #   :fixed  - do not apply page transition to this element, see
+  #             http://jquerymobile.com/demos/1.0/docs/pages/touchoverflow.html
+  #   :tag    - you don't want a div?
+  def mobile_tag(options = {}, &block)
+    options.reverse_merge!('data-role' => options.delete(:role))   if options.has_key?(:role)
+    options.reverse_merge!('data-position' => 'fixed')             if options.delete(:fixed)
+    options.reverse_merge!('data-theme' => options.delete(:theme)) if options.has_key?(:theme)
+    tag_name = options.delete(:tag) || 'div'
+    content_tag tag_name, options, &block
+  end
+end
+
--- lib/jquery_mobile-rails.rb
+++ lib/jquery_mobile-rails.rb
@@ -1,6 +1,11 @@
 module JqueryMobile
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'jquery_mobile.include_helper' do
+        ApplicationController.class_eval do
+          include JqueryMobile::TagHelper
+        end
+      end
     end
   end
 end
