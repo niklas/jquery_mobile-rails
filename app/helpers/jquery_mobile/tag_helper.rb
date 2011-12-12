ad5ab4d7460c5eed961d26a8f03098bf0cdc060c
Niklas Hofer <niklas+dev@lanpartei.de>
2011-12-12 14:51:34 +0100

    lazy accessors for options; extra_page, back_button_to

--- app/helpers/jquery_mobile/tag_helper.rb
+++ app/helpers/jquery_mobile/tag_helper.rb
@@ -3,26 +3,34 @@ module JqueryMobile::TagHelper
 
   # put this into your layout. It will yield your action-renderings for you
   def mobile_page(options = {})
-    @page_options = options
+    page_options.merge! options
     render 'mobile_page'
   end
 
   # adds the block contents to the JqueryMobile header
   def for_header(options={}, &block)
-    @header_options = options
+    header_options.merge! options
     content_for :header, &block
   end
 
   # adds the block contents to the JqueryMobile footer
   def for_footer(options={}, &block)
-    @footer_options = options
+    footer_options.merge! options
     content_for :footer, &block
   end
 
-  attr_reader :page_options
-  attr_reader :header_options
-  attr_reader :content_options
-  attr_reader :footer_options
+  def self.options(name)
+    module_eval <<-EOEVAL, __FILE__, __LINE__
+      def #{name}_options
+        @#{name}_options ||= {}
+      end
+    EOEVAL
+  end
+
+  options :page
+  options :header
+  options :content
+  options :footer
 
   # Renders a JqueryMobile specific tag
   # addition options
@@ -38,6 +46,20 @@ module JqueryMobile::TagHelper
     options.reverse_merge!('data-theme' => options.delete(:theme)) if options.has_key?(:theme)
     tag_name = options.delete(:tag) || 'div'
     content_tag tag_name, options, &block
+  end
+
+  def page(options = {}, &block)
+    mobile_tag(options.merge(:role => 'page'), &block)
+  end
+
+  def extra_page(options ={}, &block)
+    content_for :extra_pages do
+      page options, &block
+    end
+  end
+
+  def back_button_to(label, url, options = {})
+    link_to label, url, options.merge('data-rel' => 'back')
   end
 end
 
